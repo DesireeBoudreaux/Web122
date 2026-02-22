4726e4e91f439910dfd62b481c202f868f7fcc5a
Desiree <155199304+DesireeBoudreaux@users.noreply.github.com>
2026-02-21 22:53:29 -0600

    Delete JohnDoesCustomCarsAndCycles directory

--- JohnDoesCustomCarsAndCycles/js/script.js
+++
@@ -1,8 +0,0 @@
-//Hamburger menu
-const toggleButton = document.querySelector('.menu-toggle');
-const navList = document.querySelector('.nav-list');
-
-toggleButton.addEventListener('click', () => {
-    const isOpen = navList.classList.toggle('open');
-    toggleButton.setAttribute('aria-expanded', isOpen);
-});
